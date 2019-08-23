59066b4a9180cf6289138f341955962a592c7367
Matt Ray <matthewhray@gmail.com>
2019-08-23 14:46:35 +1000

    first pass

---
+++ controls/example.rb
@@ -0,0 +1,14 @@
+title "check Chef tags"
+
+# Chef node name as an input
+nodename = input('nodename', value: `hostname`.split('.')[0])
+
+# you add controls here
+control "Check Chef tags" do
+  impact 1.0
+  title "green/brown tags"
+  desc "Check if the Chef tag is green or brown (success) or missing (failure)"
+  describe command("knife tag list #{nodename} -c /etc/chef/client.rb") do
+    its('stdout') { should match (/green|brown/) }
+  end
+end

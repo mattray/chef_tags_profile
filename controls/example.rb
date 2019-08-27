a6fcebec55c7be5a0a9b92042fc1047d4d2d6f92
Matt Ray <matthewhray@gmail.com>
2019-08-27 17:01:24 +1000

    remove the default?

--- controls/example.rb
+++ controls/example.rb
@@ -1,14 +1,15 @@
 title "check Chef tags"
 
 # Chef node name as an input
-nodename = attribute('nodename', default: `hostname`.split('.')[0].strip)
+# nodename = attribute('nodename', default: `hostname`.split('.')[0].strip)
+name = attribute('nodename')
 
 # you add controls here
 control "Check Chef tags" do
   impact 1.0
   title "green/brown tags"
   desc "Check if the Chef tag is green or brown (success) or missing (failure)"
-  describe command("knife tag list #{nodename} -c /etc/chef/client.rb") do
+  describe command("knife tag list #{name} -c /etc/chef/client.rb") do
     its('stdout') { should match (/green|brown/) }
   end
 end
